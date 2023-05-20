df146d878328f363514f7997b4252720159fb104
Drey_Oroceo <adrian@tictocgames.com>
2023-05-20 23:39:34 +0800

    readded ability and talent trees, remove editor and runtime asmdefs

--- Assets/UPM Package/Runtime/Ability.cs
+++ Assets/UPM Package/Runtime/Ability.cs
@@ -5,27 +5,43 @@ using UnityEngine;
 
 namespace GAS
 {
-    [System.Serializable]
-    public class Ability
+    public abstract class Ability : ScriptableObject
     {
         public string Name => _name;
         public string Description => _description;
-        public int Cost
-        {
+        
+        /// <summary>
+        /// Skill cost for mana
+        /// </summary>
+        public float Cost
+        {   
             get => _cost;
             set => _cost = Mathf.Max(0,value);
         }
 
+        /// <summary>
+        /// skill cooldown
+        /// </summary>
         public float Cooldown
         {
             get => _cooldown;
             set => _cooldown = Mathf.Max(0f, value);
         }
+        
+        /// <summary>
+        /// skill range
+        /// </summary>
+        public float Range
+        {
+            get => _range;
+            set => _range = Mathf.Max(0f, value);
+        }
 
-        [SerializeField] private string _name;
-        [SerializeField] private string _description;
-        [SerializeField] private int _cost;
-        [SerializeField] private float _cooldown;
+        [SerializeField] protected string _name;
+        [SerializeField] protected string _description;
+        [SerializeField] protected float _cost;
+        [SerializeField] protected float _cooldown;
+        [SerializeField] protected float _range;
 
         public Ability()
         {
@@ -33,6 +49,43 @@ namespace GAS
             _description = "New Ability Description";
             _cost = 0;
             _cooldown = 0f;
+        }
+    }
+    
+    public abstract class SingleTargetAbility : Ability
+    {
+        [SerializeField] private GameObject _projectilePrefab;
+        
+        public virtual void Activate(GameObject caster, GameObject target)
+        {
+            // activate vfx
+        }
+    }
+    
+    public abstract class AoeAbility : Ability
+    {
+        [SerializeField] private GameObject _projectilePrefab;
+        
+        public virtual void Activate(GameObject caster, GameObject target)
+        {
+            //activate vfx
+        }
+    }
+    
+    public class HealAndDamage : SingleTargetAbility
+    {
+        public float Damage
+        {
+            get => _damage;
+            set => _damage = value; // if this is negative this means that this heals
+        }
+        
+        [SerializeField] private float _damage;
+        
+        public override void Activate(GameObject caster, GameObject target)
+        {
+            // activate vfx
+            // apply damage
         }
     }
 }
--- Assets/UPM Package/Runtime/Talent.cs
+++ Assets/UPM Package/Runtime/Talent.cs
@@ -5,12 +5,64 @@ using UnityEngine;
 
 namespace GAS
 {
-    [System.Serializable]
+    
     public abstract class Talent
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public int LevelRequirement
+        {
+            get { return _levelRequirement; }
+            set { _levelRequirement = value; }
+        }
+
+        [SerializeField] protected string _name;
+        [SerializeField] protected string _description;
+        [SerializeField] protected int _levelRequirement;
 
         public abstract void ApplyModifier(Ability ability);
     }
+    
+    public class RangeTalent : Talent
+    {
+        public float RangeModifier
+        {
+            get { return _rangeModifier; }
+            set { _rangeModifier = value; }
+        }
+        
+        [SerializeField] private float _rangeModifier;
+
+        public override void ApplyModifier(Ability ability)
+        {
+            ability.Range += _rangeModifier;
+        }
+    }
+    
+    public class CostTalent : Talent
+    {
+        public float CostModifier
+        {
+            get { return _costModifier; }
+            set { _costModifier = value; }
+        }
+        
+        [SerializeField] private float _costModifier;
+
+        public override void ApplyModifier(Ability ability)
+        {
+            ability.Cost *= _costModifier;
+        }
+    }
+    
 }
